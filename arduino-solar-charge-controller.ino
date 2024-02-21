7b0f539cc87784e886c6589a52b4b03fa85e8f8d
Alexandru Robert-Mihai <111703172+ARobertM@users.noreply.github.com>
2024-02-21 20:47:11 +0200

    Update arduino-solar-charge-controller.ino

--- arduino-solar-charge-controller.ino
+++ arduino-solar-charge-controller.ino
@@ -1,11 +1,23 @@
+// Battery Analog Read - 3.7V - A0 pin
+#define batteryPin A0 
+const float batteryVoltageMax = 3.7; 
+const float voltageConversionFactor = 5.0 / 1023.0; 
+
+float analogReading = 0; 
+float batteryVoltage = 0; 
+
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
-  Serial.print("salut!\n");
-  delay(2000);
-  Serial.print("Hai ca l am conectat\n");
-
+  analogReading = analogRead(batteryPin); 
+  batteryVoltage = analogReading * voltageConversionFactor * (batteryVoltageMax / 3.7); 
+  
+  Serial.print("Battery Voltage: "); 
+  Serial.print(batteryVoltage, 2); 
+  Serial.println(" V"); 
+  
+  delay(1000); 
 }
+
