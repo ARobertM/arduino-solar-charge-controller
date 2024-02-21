97c1c89a1e5d0b5aba8e5cd08c1308fd4bf36be8
Alexandru Robert-Mihai <111703172+ARobertM@users.noreply.github.com>
2024-02-21 21:30:47 +0200

    Update arduino-solar-charge-controller.ino

--- arduino-solar-charge-controller.ino
+++ arduino-solar-charge-controller.ino
@@ -21,3 +21,26 @@ void loop() {
   delay(1000); 
 }
 
+// SolarPanel alg
+// #define solarPanelPin A0 // Pinul analogic conectat la panoul solar
+// const float voltageConversionFactor = 5.0 / 1023.0; // Factorul de conversie pentru transformarea citirii analogice în tensiune (V pe contor)
+
+// float analogReading = 0; // Variabila pentru stocarea citirii analogice
+// float solarPanelVoltage = 0; // Variabila pentru stocarea tensiunii de la panoul solar
+
+// void setup() {
+//   Serial.begin(9600); // Inițializarea comunicării seriale
+// }
+
+// void loop() {
+//   analogReading = analogRead(solarPanelPin); // Citirea valorii analogice de la pinul panoului solar
+//   solarPanelVoltage = analogReading * voltageConversionFactor; // Calcularea tensiunii de la panoul solar
+  
+//   Serial.print("Solar Panel Voltage: "); // Afișarea etichetei pentru tensiunea panoului solar
+//   Serial.print(solarPanelVoltage, 2); // Afișarea tensiunii panoului solar cu două zecimale
+//   Serial.println(" V"); // Afișarea unității de măsură (volți)
+  
+//   delay(1000); // Intârziere pentru a reduce frecvența de actualizare
+// }
+
+
