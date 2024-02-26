5acb48dc9e9880f85d49fca89b346081a455f6c1
Alexandru Robert-Mihai <111703172+ARobertM@users.noreply.github.com>
2024-02-26 19:59:29 +0200

    Update arduino-solar-charge-controller.ino
    battery and solar panel read V

--- arduino-solar-charge-controller.ino
+++ arduino-solar-charge-controller.ino
@@ -1,46 +1,85 @@
-// Battery Analog Read - 3.7V - A0 pin
+#include <LiquidCrystal.h>
+
 #define batteryPin A0 
+#define solarPanelPin A1 
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 const float batteryVoltageMax = 3.7; 
 const float voltageConversionFactor = 5.0 / 1023.0; 
 
-float analogReading = 0; 
+float batteryAnalogReading = 0; 
 float batteryVoltage = 0; 
 
+float solarPanelAnalogReading = 0;
+float solarPanelVoltage = 0;
+
 void setup() {
+  lcd.begin(16, 2);
   Serial.begin(9600);
+  
+  
+  displayTitle("Made by","Robert");
+  delay(2000);
+  displayTitle("Solar Power", "Manager");
+  delay(2000);
 }
 
 void loop() {
-  analogReading = analogRead(batteryPin); 
-  batteryVoltage = analogReading * voltageConversionFactor * (batteryVoltageMax / 3.7); 
+  // analog
+  batteryAnalogReading = analogRead(batteryPin);
+  batteryVoltage = batteryAnalogReading * voltageConversionFactor * (batteryVoltageMax / 3.7); 
   
+  solarPanelAnalogReading = analogRead(solarPanelPin);
+  solarPanelVoltage = solarPanelAnalogReading * voltageConversionFactor;
+
+  
+  float batteryPercentage = (batteryVoltage / batteryVoltageMax) * 100;
+  
+  // Display values on LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Bat:");
+  lcd.print(batteryVoltage, 2);
+  lcd.print("V (");
+  lcd.print(batteryPercentage, 1);
+  lcd.print("%)");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Sol:");
+  lcd.print(solarPanelVoltage, 2);
+  lcd.print("V");
+  
+  // Display values on Serial Monitor
   Serial.print("Battery Voltage: "); 
   Serial.print(batteryVoltage, 2); 
+  Serial.println(" V"); 
+
+  Serial.print("Battery Percentage: "); 
+  Serial.print(batteryPercentage, 1); 
+  Serial.println(" %");
+
+  Serial.print("Solar Panel Voltage: "); 
+  Serial.print(solarPanelVoltage, 2); 
   Serial.println(" V"); 
   
   delay(1000); 
 }
 
-// SolarPanel alg
-// #define solarPanelPin A0 // Pinul analogic conectat la panoul solar
-// const float voltageConversionFactor = 5.0 / 1023.0; // Factorul de conversie pentru transformarea citirii analogice în tensiune (V pe contor)
-
-// float analogReading = 0; // Variabila pentru stocarea citirii analogice
-// float solarPanelVoltage = 0; // Variabila pentru stocarea tensiunii de la panoul solar
-
-// void setup() {
-//   Serial.begin(9600); // Inițializarea comunicării seriale
-// }
-
-// void loop() {
-//   analogReading = analogRead(solarPanelPin); // Citirea valorii analogice de la pinul panoului solar
-//   solarPanelVoltage = analogReading * voltageConversionFactor; // Calcularea tensiunii de la panoul solar
-  
-//   Serial.print("Solar Panel Voltage: "); // Afișarea etichetei pentru tensiunea panoului solar
-//   Serial.print(solarPanelVoltage, 2); // Afișarea tensiunii panoului solar cu două zecimale
-//   Serial.println(" V"); // Afișarea unității de măsură (volți)
-  
-//   delay(1000); // Intârziere pentru a reduce frecvența de actualizare
-// }
-
-
+void displayTitle(String line1, String line2) {
+  lcd.clear();
+  int length1 = line1.length();
+  int length2 = line2.length();
+  int start1 = (16 - length1) / 2;
+  int start2 = (16 - length2) / 2;
+  for (int i = 0; i < length1; ++i) {
+    lcd.setCursor(start1 + i, 0);
+    lcd.print(line1.charAt(i));
+    delay(200);
+  }
+  for (int i = 0; i < length2; ++i) {
+    lcd.setCursor(start2 + i, 1);
+    lcd.print(line2.charAt(i));
+    delay(200);
+  }
+}
